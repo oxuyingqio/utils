5e076d5c801a834b176be218d633f4dbb35514cc
oxuyingqio <oxuyingqio@qq.com>
2016-04-11 11:06:05 +0800

    XuYQ

--- src/test/java/cn/xuyingqi/util/JUnitTest.java
+++ src/test/java/cn/xuyingqi/util/JUnitTest.java
@@ -1,5 +1,7 @@
 package cn.xuyingqi.util;
 
+import java.text.DecimalFormat;
+
 import org.apache.log4j.Logger;
 
 public class JUnitTest {
@@ -9,5 +11,9 @@ public class JUnitTest {
 	@org.junit.Test
 	public void test() {
 		logger.debug(ByteUtils.convertSpecifiedLocation2Int(new int[] { 0, 2, 3, 31 }));
+
+		DecimalFormat df = new DecimalFormat("######0.00000000000000000");
+		System.out.println(df.format(Math.pow(1.1, 365)));
+		System.out.println(df.format(Math.pow(0.9, 365)));
 	}
 }
