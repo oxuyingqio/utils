b0543efd4c42b44042b0a7aa7480823c6526a5cd
oxuyingqio <oxuyingqio@qq.com>
2016-04-14 11:27:42 +0800

    X

---
+++ src/main/java/cn/xuyingqi/util/utils/StringUtils.java
@@ -0,0 +1,39 @@
+package cn.xuyingqi.util.utils;
+
+/**
+ * 字符串工具类
+ * 
+ * @author XuYQ
+ *
+ */
+public class StringUtils {
+
+	/**
+	 * 判断字符串是否为空
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
+
+	/**
+	 * 判断字符串是否为空或仅为空格
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isBlank(String str) {
+		int strLen;
+		if (str == null || (strLen = str.length()) == 0) {
+			return true;
+		}
+		for (int i = 0; i < strLen; i++) {
+			if ((Character.isWhitespace(str.charAt(i)) == false)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
